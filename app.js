0c9d89150a4167ff47d95dcbf633aca8c9a7f08b
xFijo <75216577+xFijo@users.noreply.github.com>
2021-07-30 20:59:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/main.html`);
 });
 
+app.get('/css', (req, res) => {
+    res.sendFile(`${__dirname}/style.css`);
+});
+
 app.listen(3000, () => {
   console.log('Online');
 });
